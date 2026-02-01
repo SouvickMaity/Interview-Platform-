16f3a36d9ed151d60d4ee2f829f75c839bb7f401
Souvick Maity <souvick662@gmail.com>
2026-02-01 15:50:02 +0530

    deployment config eddit

--- Backend/src/lib/env.js
+++ Backend/src/lib/env.js
@@ -1,8 +1,16 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ quiet: true });
 
 export const env={
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
-    
+    NODE_ENV:process.env.NODE_ENV,
+    CLIENT_URL:process.env.CLIENT_URL,
+    INNGEST_EVENT_KEY:process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY:process.env.INNGEST_SIGNING_KEY,
+    STREAM_APT_KEY:process.env.STREAM_APT_KEY,
+    STREAM_API_SECRET:process.env.STREAM_API_SECRET,
+   
 }
+
+
